7ac1259e3e290454bbc2970d68e9bb9f30fb8a07
Randika Dilshan <rdilshan077788@gmail.com>
2024-09-28 16:56:00 +0530

    simple websocket create

--- Server/index.js
+++ Server/index.js
@@ -2,11 +2,34 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.send('Hello World!');
+const http = require('http'); 
+const WebSocket = require('ws');
+
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server});
+
+wss.on('connection', (ws) => {
+    console.log('New client connected');
+
+    ws.on('message', (message) => {
+        console.log('Received:', message);
+     
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`Broadcast: ${message}`);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
 });
 
-app.listen(3000, () => {
+
+server.listen(3000, () => {
    console.log('Server is running on http://localhost:3000');
 });
 
